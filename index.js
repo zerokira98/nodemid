a842720e12893863c6903446cb629b8b31ffb95e
Rzaf98 <91272276+Rzaf98@users.noreply.github.com>
2024-07-03 15:56:20 +0700

    make it better

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+
+import express from 'express'
+
+const app = express()
+
+const PORT = process.env.PORT || 5001
+
+app.get('/', function (req, res) {
+    res.send('Hello World tai')
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
